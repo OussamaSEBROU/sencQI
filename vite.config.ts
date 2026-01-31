d7ff051f615615f127997a7044f8ff6ae024daf7
Mr. Oussama Sebrou <90466380+OussamaSEBROU@users.noreply.github.com>
2026-01-31 17:33:30 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 
 import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
-
 export default defineConfig(({ mode }) => {
   // تحميل متغيرات البيئة من النظام (Render)
   const env = loadEnv(mode, process.cwd(), '');
@@ -14,6 +13,9 @@ export default defineConfig(({ mode }) => {
     build: {
       outDir: 'dist',
       sourcemap: false
+    },
+    preview: {
+      allowedHosts: true
     }
   };
 });
